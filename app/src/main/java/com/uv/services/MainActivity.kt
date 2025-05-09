674fec04a1e87642e80c2f7f0d0f8f9d67db8975
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2025-05-09 14:48:45 -0600

    fetchUsers->ListView

--- app/src/main/java/com/uv/services/MainActivity.kt
+++ app/src/main/java/com/uv/services/MainActivity.kt
@@ -1,94 +1,74 @@
 package com.uv.services
 
-import android.annotation.SuppressLint
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.view.MotionEvent
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.android.volley.Request
-import com.android.volley.toolbox.ImageRequest
-import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
-import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textView: TextView
-    private lateinit var imageViewCat: ImageView
-    private val factUrl = "https://meowfacts.herokuapp.com/"
-    private val imageUrl = "https://cataas.com/cat"
+    private lateinit var listView: ListView
+    private val userList = ArrayList<Map<String, String>>()
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val rootView = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.main)
-
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        textView = findViewById(R.id.text)
-        imageViewCat = findViewById(R.id.imageViewCat)
-
-        fetchCatData()
-
-        rootView.setOnTouchListener { _, event ->
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                fetchCatData()
-            }
-            true
-        }
+        listView = findViewById(R.id.listViewUsers)
+        fetchUsers()
     }
 
-    private fun fetchCatData() {
-        val requestQueue = Volley.newRequestQueue(this)
+    private fun fetchUsers() {
+        val url = "https://jsonplaceholder.typicode.com/users"
+        val queue = Volley.newRequestQueue(this)
 
-        val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.GET,
-            factUrl,
-            null,
+        val req = JsonArrayRequest(
+            Request.Method.GET, url, null,
             { response ->
-                try {
-                    val dataArray = response.getJSONArray("data")
-                    val fact = dataArray.getString(0)
-                    textView.text = fact
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-                    textView.text = getString(R.string.error_processing_response)
+                for (i in 0 until response.length()) {
+                    val user: JSONObject = response.getJSONObject(i)
+                    val name = user.getString("name")
+                    val email = user.getString("email")
+                    val city = user.getJSONObject("address").getString("city")
+
+                    val map = HashMap<String, String>()
+                    map["title"] = "$name - $city"
+                    map["subtitle"] = email
+
+                    userList.add(map)
                 }
+
+                val adapter = SimpleAdapter(
+                    this,
+                    userList,
+                    android.R.layout.simple_list_item_2,
+                    arrayOf("title", "subtitle"),
+                    intArrayOf(android.R.id.text1, android.R.id.text2)
+                )
+
+                listView.adapter = adapter
             },
             { error ->
                 error.printStackTrace()
-                textView.text = getString(R.string.request_error)
+                Toast.makeText(this, "Error at fetch users", Toast.LENGTH_SHORT).show()
             }
         )
 
-        val imageRequest = ImageRequest(
-            imageUrl,
-            { bitmap: Bitmap ->
-                imageViewCat.setImageBitmap(bitmap)
-            },
-            0,
-            0,
-            ImageView.ScaleType.CENTER_CROP,
-            Bitmap.Config.ARGB_8888,
-            { _ ->
-                Toast.makeText(this, "Error al cargar la imagen.", Toast.LENGTH_SHORT).show()
-            }
-        )
-
-        requestQueue.add(jsonObjectRequest)
-        requestQueue.add(imageRequest)
+        queue.add(req)
     }
 }
